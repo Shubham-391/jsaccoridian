ed6ba1f42f9ca5a6b2f1d64dde4eb2185398e83f
Shubham-391 <subhamy1234@gmail.com>
2023-08-27 15:02:53 +0530

    commit

---
+++ assets/js/doc.js
@@ -0,0 +1,14 @@
+const accordianItems = document.querySelectorAll(".tab");
+
+accordianItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+        const isItemOpen = item.classList.contains("active"); //returns false
+        accordianItems.forEach(function (item) { item.classList.remove("active") });
+        if (!isItemOpen) //returns true
+        {
+            item.classList.toggle("active");
+        }
+    })
+});
+
+
